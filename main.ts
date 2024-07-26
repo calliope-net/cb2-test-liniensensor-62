101133e67ffbf298c0e83fbecb28c9ff59c9a969
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-07-26 22:24:54 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     Wendekreis_cm = 8 * btf.pi()
-    btf.comment(btf.btf_text("viertel Drehung"))
+    btf.comment(btf.btf_text("viertel Drehung Umfang/4"))
     cb2.fahre2MotorenEncoder(
     btf.speedPicker(30),
     btf.speedPicker(-30),
@@ -13,6 +13,9 @@ input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
     hex2 = i2c.i2ctohex(i2c.i2c_eADDR(i2c.eADDR.Relay_x1E), i2c.i2c_eADDR(i2c.eADDR.RGB_16x2_x30), 100)
     basic.showString(hex2)
     basic.showNumber(hex2.length / 2)
+})
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    dauerhaft_Knopf_B = !(dauerhaft_Knopf_B)
 })
 input.onButtonEvent(Button.B, btf.buttonEventValue(ButtonEvent.Hold), function () {
     btf.zeigeBIN(bit.hex8(bit.eHEX8bit.x22), btf.ePlot.hex, 4)
@@ -38,9 +41,30 @@ input.onButtonEvent(Button.A, btf.buttonEventValue(ButtonEvent.Hold), function (
         }
     }
 })
+let bWiederholung = false
+let dauerhaft_Knopf_B = false
 let hex2 = ""
 let Wendekreis_cm = 0
 cb2.writeReset()
 btf.zeigeBIN(cb2.readSpannung(), btf.ePlot.bcd, 1)
 btf.zeigeBIN(cb2.readVersionArray()[1], btf.ePlot.bcd, 2)
 btf.comment(btf.btf_text("calliope-net/cb2-test-liniensensor-62"))
+basic.forever(function () {
+    if (dauerhaft_Knopf_B) {
+        cb2.beispielSpurfolger16(
+        192,
+        160,
+        31,
+        0,
+        bWiederholung,
+        true,
+        20,
+        cb2.eI2C.x22
+        )
+        bWiederholung = true
+    } else if (bWiederholung) {
+        dauerhaft_Knopf_B = false
+        bWiederholung = false
+        cb2.writeMotorenStop()
+    }
+})
